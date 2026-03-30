59222442427701958681de931897ff11ef4ee459
siyar3738 <siyar3738@gmail.com>
2026-03-30 11:13:36 +0530

    30 march

--- 30 march/script.js
+++ 30 march/script.js
@@ -1,6 +1,19 @@
-No_of_entry = Number(prompt("Enter the Number of Values you want to Insert in the Array: "))
+// a=String(prompt("enter the value in array"))
+// for(i=0;i<=a;i++){
+//     c=String(prompt("enter the "+ i + "value for array"))
+// console.log()}
+// let b=["siya","riya","jiya"]
+// b.push("piya")
+// console.log(b);
 
 
-for (i=0;i<=No_of_entry;i++){
-    prompt("Enter the "+ i + " Value for Array")
-}
+
+
+// let names=["riya","jiya"]
+// names.push("piya")
+// console.log(names);
+
+// let Names=["riya","jiya","piya"]
+// let removed = Names.pop();
+// console.log(Names);
+// console.log(removed);

9211f6687e2a63d0d47fa1436ff20b63e30ed154
siyar3738 <siyar3738@gmail.com>
2026-03-30 21:13:54 +0530

    home libarary

--- libarary/circle.js
+++ libarary/circle.js
@@ -1,9 +1,9 @@
-export function Area(r) {
-    Area=3.14*r*r
-    return (3.14*r*r);
-}
+// export function Area(r) {
+//     Area=3.14*r*r
+//     return (3.14*r*r);
+// }
 
-export function Perimeter(r) {
-    Perimeter=2*3.14*r
-    return (2*3.14*r);
-}
+// export function Perimeter(r) {
+//     Perimeter=2*3.14*r
+//     return (2*3.14*r);
+// }

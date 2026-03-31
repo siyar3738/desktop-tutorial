b01379a4540edc290f46bdaab1428a183deeeba1
siyar3738 <siyar3738@gmail.com>
2026-03-31 10:48:53 +0530

    31 march

---
+++ 31 march/script.js
@@ -0,0 +1,21 @@
+// limit = prompt(" enter the number if element you want to insert in the Array: ")
+
+// Array=[]
+
+// for(i=0;i<=limit;i++){
+//     x=prompt("enter the elements of array : ")
+//     Array.push(x)
+// }
+// console.log(Array)
+
+
+
+x=(prompt("Enter the number"))
+for(i=0;i<=x;i++){
+   n=10
+    n+=i
+}console.log(n)
+    
+
+
+

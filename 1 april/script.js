127352b6245ff1efe49be7da7642923e29dc6ee8
siyar3738 <siyar3738@gmail.com>
2026-04-01 11:36:02 +0530

    1 april

---
+++ 1 april/script.js
@@ -0,0 +1,12 @@
+// y=prompt("whch object from starting do you want to see")
+// x=prompt("which object ending do you want to see")
+// for(i=y;i<=x;i++){
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log(data[i]);
+//    })
+//   .catch(error => {
+//     console.error('Error fetching data:', error);
+//   });
+// }

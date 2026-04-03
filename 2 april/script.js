9c5c624ecf550ed99e17e34279b2227470aa0ab1
siyar3738 <siyar3738@gmail.com>
2026-04-03 09:38:33 +0530

    2 april 3 april

--- 2 april/script.js
+++ 2 april/script.js
@@ -1,5 +1,4 @@
-x=Number(prompt("Enter the number the table"))
-limit=Number(prompt("enter the limit of table"))
-for(i=1;i<=limit;i++)
-console.log(x+"x"+i,"=",x*i)
-
+    // x=Number(prompt("Enter the number the table"))
+    // limit=Number(prompt("enter the limit of table"))
+    // for(i=1;i<=limit;i++)
+    // console.log(x+"x"+i,"=",x*i)

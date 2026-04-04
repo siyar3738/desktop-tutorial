e9ecc56f5335d87fb3bdcc4d7318c54a8a968e10
siyar3738 <siyar3738@gmail.com>
2026-04-04 11:07:10 +0530

    4 april

---
+++ 4 april/script.js
@@ -0,0 +1,23 @@
+// A=[0,1,2,3,4,5,6,7,8,9];
+// B=[0,1,2,3,4,5,6,7,8,9];
+// let a=A[Math.floor(Math.random()*A.length)];
+// let b=B[Math.floor(Math.random()*B.length)];
+// let ans =Number(prompt("What is " + a + " + " + b + "?"));
+// if(a+b===ans){
+//     console.log("Answer is right")
+// }else{
+//     console.log("Answer is wrong")
+// }
+
+
+
+
+
+
+
+// A=[0,1,2,3,4,5,6,7,8,9];
+// let a=A[Math.floor(Math.random()*A.length)];
+
+// a=A[4]
+
+// console.log(a)

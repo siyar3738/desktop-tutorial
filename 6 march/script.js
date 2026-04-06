f8f63e444ff0579c29924c79107d52dd1d82e012
siyar3738 <siyar3738@gmail.com>
2026-04-06 11:28:54 +0530

    3 april 6 march libaray

---
+++ 6 march/script.js
@@ -0,0 +1,36 @@
+// let button = document.getElementById("x");
+// button.onclick = function() {
+//   alert("Button clicked");
+  
+  
+//   for(i=1;i<=10;i++){
+  
+  
+  
+//   setTimeout(() => {
+//   console.log(i);
+// },1000);
+
+//   }
+
+
+
+
+// }
+
+
+
+//  Indification={
+//      "Name":"Siya thakur",
+//      "Fathers name":"Rajvarinder singh",
+//      "Mothers  name":"Veena  devi",
+//      "Cast":"Gernal",
+//      "Gender":"Female",
+//      "function":function Perimeter(r) {
+//     Perimeter=2*3.14*r
+//     return (2*3.14*r);
+// }
+// }
+// result=Indification.function(2)
+
+// console.log(result)

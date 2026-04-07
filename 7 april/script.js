f974573f62367771c21297bfacb93f4d01522da0
siyar3738 <siyar3738@gmail.com>
2026-04-07 11:32:38 +0530

    7 april

---
+++ 7 april/script.js
@@ -0,0 +1,25 @@
+// x = document.getElementById ("counting")
+
+// for(i=0;i<=11;i++){
+//    x.innerHTML=i
+//    console.log(i)
+// }
+
+// x = document.getElementById ("a")
+// let button = document.getElementById("a");
+//  button.onclick = function() {
+// x.innerHTML="clicked buttton"}
+
+
+// function startCount() {
+//   for (let i = 10; i >= 0; i--) {
+//     setTimeout(function() {
+         
+//       if (i === 0) {
+//         console.log("Time's up!");
+//       }
+//       document.getElementById("counting").innerHTML = i;
+//     }, (10-i) * 1000);
+//   }
+  
+// }

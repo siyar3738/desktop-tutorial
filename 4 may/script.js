9f046d0b78424054f0e6c45e4ae2aac0cc37da4c
siyar3738 <you@example.com>
2026-05-08 09:33:02 +0530

    efgrfh

---
+++ 4 may/script.js
@@ -0,0 +1,22 @@
+// let command = {};
+// for(let i=1;i<=10;i++) {
+//     let x = prompt('enter the command ${i}:');
+//     command[`command-${i}`] = x;
+// }
+// console.log(command)
+
+
+
+
+// let command = {};
+// let i = 1; 
+
+// while (i <= 10) {   
+//     let x = prompt(`enter the command ${i}:`);
+//     command[`command-${i}`] = x;
+//     i++;  
+// }
+// console.log(command);
+
+
+
